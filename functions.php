00a8afd46fcbfd1a9dea3bd8023c7071d8c4dd97
Mark <58403711+MJBrittonSolutions@users.noreply.github.com>
2020-07-05 22:20:08 +0100

    v 0.1.3 front end bloxks styles removal

--- functions.php
+++ functions.php
@@ -66,4 +66,12 @@ $urls = array_diff( $urls, array( $emoji_svg_url ) );
 return $urls;
 }
 
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+ wp_dequeue_style( 'wp-block-library' );
+ wp_dequeue_style( 'wp-block-library-theme' );
+ wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+}
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+
 ?>
