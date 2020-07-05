ce43cd9c7cf0f8fee9e4c5b54cc1520e7d988286
Mark <58403711+MJBrittonSolutions@users.noreply.github.com>
2020-07-05 23:42:16 +0100

    v 0.1.5

---
+++ func/disable-blocks.php
@@ -0,0 +1,11 @@
+<?php 
+// Fully Disable Gutenberg editor.
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
+
+function smartwp_remove_wp_block_library_css(){
+ wp_dequeue_style( 'wp-block-library' );
+ wp_dequeue_style( 'wp-block-library-theme' );
+ wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+}
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
+?>
