9e402997dcd920b54ff890b9dad5b408aa9bf344
Mark <58403711+MJBrittonSolutions@users.noreply.github.com>
2020-07-05 21:17:00 +0100

    v 0.1.0 holiday page

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php get_header(); ?>
 
 <div class="container">
-    <div class="logo">
-    <?php echo file_get_contents( get_stylesheet_directory_uri() . '/images/logo.svg' ); ?>
+    <div class="holding-page__content">
+        <div class="logo">
+                <?php echo file_get_contents( get_stylesheet_directory_uri() . '/images/logo.svg' ); ?>
+        </div>
+        <h1 class="holding-page__header">Welcome to my website!</h1>
+        <p class="holding-page__text">You may have noticed it isn't quite ready yet.</p>
+        <p class="holding-page__text">In the mean time please do get in touch to discuss how I can help you.</p>
+        <a class="holding-page__link" href="mailto:hello@mjbritton.co.uk"><span class="holding-page__button animation__heartbeat"><?php echo file_get_contents( get_stylesheet_directory_uri() . '/images/icons/envelope.svg' ); ?></span></a>
     </div>
-    <h1>This is a test to show things are working!</h1>
 </div>
 
 <?php get_footer(); ?>
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
-import "../style/app.sass"
-import 'jquery'
+import '../style/app.sass';
+import 'jquery';
 
 // Allow new JS and CSS to load in browser without a traditional page refresh
 if (module.hot) {
-    module.hot.accept()
-  }
+	module.hot.accept();
+}
