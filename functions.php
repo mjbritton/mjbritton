b07415ffa4e2d2880ca97219ad8c608d0692093c
Mark <58403711+MJBrittonSolutions@users.noreply.github.com>
2020-07-05 17:36:45 +0100

    Version: 0.0.2

--- functions.php
+++ functions.php
@@ -6,8 +6,8 @@ function theme_files() {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.eb459030a4410eb492af.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-theme-js', get_theme_file_uri('/bundled-assets/scripts.66589fe72bfa3b73da14.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.66589fe72bfa3b73da14.css'));
+    wp_enqueue_script('main-theme-js', get_theme_file_uri('/bundled-assets/scripts.0be8a83883ff26c86259.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.0be8a83883ff26c86259.css'));
   }
 
   wp_localize_script('main-theme-js', 'themeData', array(
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php get_header(); ?>
 
+<div class="container">
+    <h1>This is a test to show things are working!</h1>
+</div>
+
 <?php get_footer(); ?>
