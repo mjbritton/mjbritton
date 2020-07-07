7dd1237f1df1a516f6dc377945ed0138edf7a7f0
Mark <58403711+MJBrittonSolutions@users.noreply.github.com>
2020-07-07 18:51:16 +0100

    v0.1.7

--- functions.php
+++ functions.php
@@ -7,11 +7,11 @@ require_once( __DIR__ . '/func/disable-emoji.php');
 function theme_files() {
   
   if (strstr($_SERVER['SERVER_NAME'], 'mjbritton.local')) {
-    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+    wp_enqueue_script('main-theme-js', 'http://localhost:3000/bundled.js', NULL, '1.1', true);
   } else {
     wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.eb459030a4410eb492af.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-theme-js', get_theme_file_uri('/bundled-assets/scripts.c48861fb837cb718674b.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.c48861fb837cb718674b.css'));
+    wp_enqueue_script('main-theme-js', get_theme_file_uri('/bundled-assets/scripts.b11aca12029629d9e8fb.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.b11aca12029629d9e8fb.css'));
   }
 
   wp_localize_script('main-theme-js', 'themeData', array(
--- index.php
+++ index.php
@@ -10,7 +10,8 @@
         <p class="holding-page__text">In the mean time please do get in touch to discuss how I can help you.</p>
         <a class="holding-page__link" href="mailto:hello@mjbritton.co.uk" title="Email me!"><span class="holding-page__button animation__heartbeat"><?php echo file_get_contents( get_stylesheet_directory_uri() . '/images/icons/envelope.svg' ); ?></span></a>
     
-    <div class="holding-page__copyright"><p class="holding-page__text--small">&copy MJBritton Digital and Design 2020</p></div>
+            <div class="holding-page__copyright"><p class="holding-page__text--small">&copy MJBritton Digital and Design 2020</p></div>
+
     </div>
 </div>
 
