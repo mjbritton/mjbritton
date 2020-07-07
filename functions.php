1031964392387527566fa52fa9a57acb98c28820
Mark <58403711+MJBrittonSolutions@users.noreply.github.com>
2020-07-07 17:21:21 +0100

    v 0.1.6
    made mobile friendly

--- functions.php
+++ functions.php
@@ -1,7 +1,26 @@
 <?php
 
-require_once( __DIR__ . '/func/theme-files.php');
+require_once( __DIR__ . '/func/theme-support.php');
 require_once( __DIR__ . '/func/disable-blocks.php');
 require_once( __DIR__ . '/func/disable-emoji.php');
+
+function theme_files() {
+  
+  if (strstr($_SERVER['SERVER_NAME'], 'mjbritton.local')) {
+    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+  } else {
+    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.eb459030a4410eb492af.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-theme-js', get_theme_file_uri('/bundled-assets/scripts.c48861fb837cb718674b.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.c48861fb837cb718674b.css'));
+  }
+
+  wp_localize_script('main-theme-js', 'themeData', array(
+    'root_url' => get_site_url(),
+    'nonce' => wp_create_nonce('wp_rest')
+  ));
+}
+
+add_action('wp_enqueue_scripts', 'theme_files');
+
 
 ?>
